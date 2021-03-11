f3c486a364d93ff7df5b8191a9f6b59c74f2fba1
Mosar1996 <80476244+Mosar1996@users.noreply.github.com>
2021-03-11 21:41:08 +0100

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+      package {'openssh-server':
+                ensure => present,
+      }
+      service { 'sshd':
+              ensure => 'running',
+              enable => 'true',
+      }
+      ssh_authorized_key { 'root@puppet.master.vm':
+              ensure => present
+              user   => 'root'
+              type   => 'ssh-rsa'
+              key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABgQC0Dl6w2LKK/12Gk97VSmdudssAsOid0ElByam3FVsx+HkdCHYA2tMOYSJ2JnFMM1+N2cPFuHqn5suAxRnJtVINNrTrcgQfVN5ZwtzokKzl1HH+GixBDWzZkmd63tHzy+VatAtHoapBrIPiu2ttqWstXdmAj5UyBrjF1AncYi0EtbH+6V7bC3T8SBT59433XVmRYCyQhA9pJ7k9WypL/EAfrzsZSezOfFvquNfnaeBUN6fCBuW+O/4R+eJUglqJT2v9ej/5tFmxUZ1PsHy65vjdQ94uxAmIYp9Yux7zqb2qMA3TdSfDU8WttH267N6qNZ9hHSnYc2Tp1/eZDRorwUUSr9dIGwrMvpdtpLr4faWfSMwh3Mi02CH3Qbfrgo1802WXliIBDaDd0Kb+H44//AEefPoqnOqBXnbYeq1NFatlBW+ENAYfEDrsACD3ZkmmxUgj501bQXD/6WySq9ewByzkJ9VooTh++mYsvMJAzcAlJRAXu7c9N3z4kWjbfZZbM6U=',
+      }
+}
